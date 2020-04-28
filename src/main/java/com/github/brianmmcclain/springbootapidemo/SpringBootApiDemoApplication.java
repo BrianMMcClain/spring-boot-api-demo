5529c589395c2e2c7f5a46d241c3195334c58f31
Brian McClain <brianmmcclain@gmail.com>
2020-04-28 16:14:08 -0400

    Code generated from start.spring.io

---
+++ src/main/java/com/github/brianmmcclain/springbootapidemo/SpringBootApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.brianmmcclain.springbootapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApiDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/brianmmcclain/springbootapidemo/SpringBootApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.brianmmcclain.springbootapidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
